1914df27c74cb332ee406615bf75208590c9f4ab
teks <tom.olson@gmail.com>
2025-03-25 08:54:34 -0400

    first floor API function w/test

--- src/floor.rs
+++ src/floor.rs
@@ -1,6 +1,10 @@
 /* Floor objects have 9 cells in a 3 x 3 grid.
 */
 
+#![allow(dead_code)] // not everything is implemented perfectly right away, rust, geez
+
+use std::collections::HashSet;
+
 enum CellType {
     SmallRoom,
     MediumRoom,
@@ -9,6 +13,7 @@ enum CellType {
     Empty
 }
 
+#[derive(Debug, PartialEq, Eq, Hash)] // without this line, CellLocations can't be HashMap keys
 enum CellLocation {
     NW, N, NE,
     W,  C, E,
@@ -16,10 +21,43 @@ enum CellLocation {
 }
 
 impl CellLocation {
-    fn neighbors(&self) {
+    fn neighbors(&self) -> HashSet<CellLocation> {
+        use CellLocation::*;
+        match self {
+            NW => HashSet::from([N, W]),
+            NE => HashSet::from([N, E]),
+            SE => HashSet::from([S, E]),
+            SW => HashSet::from([S, W]),
+            N  => HashSet::from([NW, C, NE]),
+            E  => HashSet::from([NE, C, SE]),
+            W  => HashSet::from([NW, C, SW]),
+            S  => HashSet::from([SW, C, SE]),
+            C  => HashSet::from([N, E, S, W]),
+            // _  => no need for this since rust detects it's exhaustive
+        }
     }
 }
 
 struct Floor {
+
+}
+
+#[cfg(test)]
+mod tests {
+    #![allow(non_snake_case)] // I know how to name things thanks
+    use super::*;
+
+    #[test]
+    fn test_CellLocation_neighbors() {
+        // test a few cases:
+        let nw_neighbors = CellLocation::NW.neighbors();
+        assert_eq!(nw_neighbors, HashSet::from([CellLocation::N, CellLocation::W]));
+
+        let c_neighbors = CellLocation::C.neighbors();
+        assert_eq!(c_neighbors, HashSet::from([CellLocation::N, CellLocation::E, CellLocation::S, CellLocation::W]));
+
+        let s_neighbors = CellLocation::S.neighbors();
+        assert_eq!(s_neighbors, HashSet::from([CellLocation::SW, CellLocation::C, CellLocation::SE]));
+    }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod floor;
+
 fn main() {
     println!("Hello, world!");
 }
